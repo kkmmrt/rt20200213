0b316918c0000f5fca2ca74bb27b313ebcf33605
rt <rt>
2020-02-13 12:16:47 +0200

    add22

--- app/src/main/java/com/example/rt20200213/MainActivity.java
+++ app/src/main/java/com/example/rt20200213/MainActivity.java
@@ -3,12 +3,27 @@ package com.example.rt20200213;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText lauk1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        lauk1 = (EditText) findViewById(R.id.laukas1);
     }
+
+    public void add22 (View view){
+        String eil = lauk1.getText().toString();
+        double sk = Double.parseDouble(eil);
+        sk=sk+22;
+        eil = Double.toString(sk);
+        lauk1.setText(eil);
+    }
+
 }
